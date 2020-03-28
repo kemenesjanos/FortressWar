fede475224ecd2e9551f405a2badc8b0507ea8ff
bekep <petra.beke003@gmail.com>
2020-03-28 22:25:59 +0100

    Added StyleCop to .Tests

--- OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Tests/Tests.cs
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Tests/Tests.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// <copyright file="Tests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 namespace FortressWar.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Tests
     {
     }
