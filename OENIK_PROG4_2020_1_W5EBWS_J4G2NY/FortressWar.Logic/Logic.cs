07abcf3bbf0003b1a06005e8ab7ff49cbf162814
bekep <petra.beke003@gmail.com>
2020-03-28 22:19:49 +0100

    Added StyleCop to .Logic

--- OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Logic/Logic.cs
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Logic/Logic.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// <copyright file="Logic.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 namespace FortressWar.Logic
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Logic
     {
     }
