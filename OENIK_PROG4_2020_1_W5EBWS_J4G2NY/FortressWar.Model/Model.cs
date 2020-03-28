ca9df22b2fd181a4947a38ba18e05f1772762f3e
bekep <petra.beke003@gmail.com>
2020-03-28 22:41:54 +0100

    Added StyleCop to .Model

--- OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Model/Model.cs
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Model/Model.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// <copyright file="Model.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 namespace FortressWar.Model
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Model
     {
     }
