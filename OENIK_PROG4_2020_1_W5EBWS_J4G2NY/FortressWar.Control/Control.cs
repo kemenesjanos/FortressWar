ac7dae110f08d27e920999bb5bc2915a36da70bf
bekep <petra.beke003@gmail.com>
2020-03-28 22:35:35 +0100

    Added StyleCop to .Control

--- OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Control/Control.cs
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Control/Control.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// <copyright file="Control.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 namespace FortressWar.Control
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Control
     {
     }
