4ccfce09124ab56f216baff970d59fd65d769701
bekep <petra.beke003@gmail.com>
2020-03-29 20:00:27 +0200

    Added Fortress.cs

---
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Model/Fortress.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Fortress.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace FortressWar.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Describing the fortresses.
+    /// </summary>
+    public class Fortress : Character
+    {
+    }
+}
