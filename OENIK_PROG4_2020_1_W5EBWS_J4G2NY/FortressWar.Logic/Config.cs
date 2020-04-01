611926f469c07d2251aa04ed43f3cf242ccbd861
bekep <petra.beke003@gmail.com>
2020-04-01 15:56:57 +0200

    Added Config.cs

---
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Logic/Config.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Config.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace FortressWar.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Config of the game.
+    /// </summary>
+    public class Config
+    {
+    }
+}
