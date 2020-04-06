ce6805364bfa1abc9005fcce0bd445ae27f6628e
kemenes_janos <kemenes.janos1999@gmail.com>
2020-04-06 15:13:02 +0200

    Filled IModel

--- OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Logic/Logic.cs
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Logic/Logic.cs
@@ -8,11 +8,73 @@ namespace FortressWar.Logic
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using FortressWar.Model;
 
     /// <summary>
     /// Logic.
     /// </summary>
-    public class Logic
+    public class Logic : ILogic
     {
+        public event EventHandler RefreshScreen;
+
+        public bool Attack(Character attackedCharacter, int damage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Die(Character character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetBonus(Soldier soldier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCoin(Players player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadGameState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveSelector(int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveSoldier(Soldier soldier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewCharacter(Characters character, Players player, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveGameState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCharacter(Characters character)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Model/IModel.cs
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Model/IModel.cs
@@ -18,5 +18,105 @@ namespace FortressWar.Model
         /// Build the map.
         /// </summary>
         void MapBuild();
+
+        /// <summary>
+        /// Gets or sets the Width of the game in pixels.
+        /// </summary>
+        double GameWidth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Height of the game in pixels.
+        /// </summary>
+        double GameHeight { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tile width in pixels.
+        /// </summary>
+        double TileWidth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tile height in pixels.
+        /// </summary>
+        double TileHeight { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first player's name.
+        /// </summary>
+        string Player_1_name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second player's name.
+        /// </summary>
+        string Player_2_name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first player's money.
+        /// </summary>
+        int Money_1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second player's money.
+        /// </summary>
+        int Money_2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of barricades in the game.
+        /// </summary>
+        List<Barricade> Barricades { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of soldiers in the game.
+        /// </summary>
+        List<Soldier> Soldiers { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of bonuses in the game.
+        /// </summary>
+        List<Bonus> Bonuses { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of monies in the game.
+        /// </summary>
+        List<Money> Monies { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first player's fortress.
+        /// </summary>
+        Fortress Fortress_1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second player's fortress.
+        /// </summary>
+        Fortress Fortress_2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first player's rider level.
+        /// </summary>
+        int RiderLVL_1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second player's rider level.
+        /// </summary>
+        int RiderLVL_2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first player's knight level.
+        /// </summary>
+        int KnightLVL_1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second player's knight level.
+        /// </summary>
+        int KnightLVL_2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first player's barricade level.
+        /// </summary>
+        int BarricadeLVL_1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second player's barricade level.
+        /// </summary>
+        int BarricadeLVL_2 { get; set; }
     }
 }
