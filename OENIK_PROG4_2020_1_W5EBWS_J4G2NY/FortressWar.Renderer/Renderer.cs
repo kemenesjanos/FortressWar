4791e6be8577c0f2810748e31156bcf1aeef0d23
bekep <petra.beke003@gmail.com>
2020-04-14 22:59:09 +0200

    Added FortressWar.Renderer

---
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Renderer/Renderer.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Renderer.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace FortressWar.Renderer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Renderer of the game.
+    /// </summary>
+    public class Renderer
+    {
+    }
+}
