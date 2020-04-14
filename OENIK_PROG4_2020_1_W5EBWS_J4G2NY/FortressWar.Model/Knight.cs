ede99c4b46e22f5ed2ccda73f2e860e3213818b6
bekep <petra.beke003@gmail.com>
2020-04-14 22:16:45 +0200

    Added Knight.cs

---
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Model/Knight.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="Knight.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace FortressWar.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Knight qualities.
+    /// </summary>
+    public class Knight : Soldier
+    {
+        /// <summary>
+        /// Calling if the character is dead.
+        /// </summary>
+        public override void Dead()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
