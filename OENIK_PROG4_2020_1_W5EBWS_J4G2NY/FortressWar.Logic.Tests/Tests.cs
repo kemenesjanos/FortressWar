d89154182344655a085cf22f77ddf4615963d4f6
kemenes_janos <kemenes.janos1999@gmail.com>
2020-04-20 21:37:33 +0200

    Prepare Logic Tests

--- OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Logic.Tests/Tests.cs
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar.Logic.Tests/Tests.cs
@@ -4,6 +4,8 @@
 
 namespace FortressWar.Logic.Tests
 {
+    using FortressWar.Model;
+    using NUnit.Framework;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -13,7 +15,67 @@ namespace FortressWar.Logic.Tests
     /// <summary>
     /// Tests for the logic.
     /// </summary>
+    [TestFixture]
     public class Tests
     {
+        public void TestAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestDie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestEndGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestGetPotion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestLoadGameState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestMoveSelector()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestMoveSoldier()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestNewCharacter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestNewExtra()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestSaveGameState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestStartGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestUpdateCharacter()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
