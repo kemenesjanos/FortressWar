7a55cb89b25755a27adf2a8eb99441c867c92fc6
bekep <petra.beke003@gmail.com>
2020-05-10 01:56:42 +0200

    Added game description (hun) to DescriptionPage.

--- OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar/MainWindow.xaml.cs
+++ OENIK_PROG4_2020_1_W5EBWS_J4G2NY/FortressWar/MainWindow.xaml.cs
@@ -44,6 +44,10 @@ namespace FortressWar
             switch (e.Key)
             {
                 case Key.Escape: this.frame.NavigationService.Navigate(new EscPage()); break;
+                    //TODO: talán az lenne a praktikusabb, ha a controlban is szerepelne egy Escape, ami meg menti (meghívja a SAVE-t) a játékot a lenyomásra
+                    //TODO: ha Resume-t nyom a játékos, akkor lényegében egy LOAD történne
+                    //TODO: nem biztos, hogy érdemes lenne piszkálni azzal a programot, hogy megállítgatjuk a TicTimer-t
+                    //TODO: főleg ha már amúgy is meg kell valósítani a Save/Load dolgot
             }
         }
     }
